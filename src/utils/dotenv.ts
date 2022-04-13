23979fe0c2dfefd33caca18a78ddc77272c5a8a3
Pablion <36828324+Pablion@users.noreply.github.com>
2022-04-13 07:24:49 +0200

    chore: add dotenv-parse-variables

--- src/utils/dotenv.ts
+++ src/utils/dotenv.ts
@@ -1,12 +1,24 @@
+// load and parse .env file
+// TODO: separate ENV file to a new variable instead of exporting all.
+// TODO: (to hide info like `npm_package_name: 'algorand-near-bridge',1`)
+
 export { loadDotEnv, ENV };
 
+import dpv from 'dotenv-parse-variables';
+
 function loadDotEnv() {
-  require('dotenv').config();
-  // TODO:(DONE) update default value of env settings
+  let env = require('dotenv').config();
+  require('dotenv-parse-variables');
+
+  return dpv(env.parsed, {
+    assignToProcessEnv: true,
+    overrideProcessEnv: true,
+    ignoreFunctions: true,
+  });
 }
 
 const default_ENV = {
-  PORT: '4190',
+  PORT: 4190,
   NEAR_CONFIRM_TIMEOUT_SEC: 60,
   NEAR_CONFIRM_INTERVAL_SEC: 5,
   ALGO_CONFIRM_TIMEOUT_SEC: 60,
@@ -29,5 +41,33 @@ const secret_ENV = {
   ALGO_EXAMPL_PASS: 'not_loaded_from_env',
   PURE_STAKE_API_KEY: 'not_loaded_from_env',
 };
-loadDotEnv();
-const ENV = { ...secret_ENV, ...default_ENV, ...process.env };
+const parsed_ENV = loadDotEnv();
+
+const ENV = { ...secret_ENV, ...default_ENV, ...process.env, ...parsed_ENV };
+
+/* TS engine doesn't know number|string
+
+const numberFields = [
+  'PORT',
+  'NEAR_CONFIRM_TIMEOUT_SEC',
+  'NEAR_CONFIRM_INTERVAL_SEC',
+  'ALGO_CONFIRM_TIMEOUT_SEC',
+  'ALGO_CONFIRM_INTERVAL_SEC',
+  'TEST_NET_GO_NEAR_ASSET_ID',
+];
+const parseIntField = (
+  env: { [k: string]: string | number },
+  fields: string[]
+) => {
+  const ret = { ...env };
+  fields.forEach((field) => {
+    if (typeof ret[field] === 'string') {
+      ret[field] = parseInt(ret[field] as string);
+    }
+  });
+  return ret;
+};
+
+let strENV = { ...secret_ENV, ...default_ENV, ...process.env };
+const ENV = parseIntField(strENV, numberFields);
+*/
