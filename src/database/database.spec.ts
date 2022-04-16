931b729d221adb235a621386506a465116793544
Pablion <36828324+Pablion@users.noreply.github.com>
2022-04-16 13:51:39 +0200

    test: CRUD test of database

--- src/database/database.spec.ts
+++ src/database/database.spec.ts
@@ -34,17 +34,69 @@ describe('database test', () => {
       expect(await postgres._connectionTest()).toBe('Hello world!');
     });
     it('create and drop a new table', async () => {
-      await postgres.connect();
       const tableName = 'test_table_fakeNonce';
       const query = `CREATE TABLE ${tableName} (
         id SERIAL PRIMARY KEY,
-        test_date TIMESTAMP NOT NULL
-      );`;
+        test_date BIGINT NOT NULL
+        );`;
+
+      await postgres.connect();
       const res = await postgres.query(query);
+      await postgres.disconnect();
+
       expect(res.length).toBe(0);
       const res2 = await postgres.query(`DROP TABLE ${tableName};`);
       expect(res2.length).toBe(0);
+    });
+    it('read and write to test_table', async () => {
+      const tableName = 'test_table';
+      const date = +new Date();
+      const query = `INSERT INTO ${tableName} (test_date) VALUES ($1);`;
+
+      await postgres.connect();
+      const res = await postgres.query(query, [date]);
+      const res2 = await postgres.query(`SELECT * FROM ${tableName};`);
       await postgres.disconnect();
+
+      expect(res.length).toBe(0);
+      expect(res2.at(-1).test_date).toBe(date.toString());
+    });
+    it('update in test_table', async () => {
+      const tableName = 'test_table';
+      const targetId = 1;
+      const date = +new Date();
+      const query = `UPDATE ${tableName} SET test_date = $1 WHERE id = $2;`;
+
+      await postgres.connect();
+      const res = await postgres.query(query, [date, targetId]);
+      const res2 = await postgres.query(
+        `SELECT * FROM ${tableName} WHERE id = $1;`,
+        [targetId]
+      );
+      await postgres.disconnect();
+
+      expect(res.length).toBe(0);
+      // Without sorting, first element in res2 has id 2.
+      expect(res2[0].test_date).toBe(date.toString());
+    });
+
+    it('delete last entry in test_table', async () => {
+      // todo: maybe just check MAX(id)?
+      const tableName = 'test_table';
+      const query = `DELETE FROM ${tableName} WHERE id = (SELECT MAX(id) FROM ${tableName});`;
+
+      await postgres.connect();
+      const res_before_del = await postgres.query(
+        `SELECT * FROM ${tableName} ;`
+      );
+      const res = await postgres.query(query);
+      const res_after_del = await postgres.query(
+        `SELECT * FROM ${tableName} ;`
+      );
+      await postgres.disconnect();
+
+      expect(res.length).toBe(0);
+      expect(res_before_del.length - res_after_del.length).toBe(1);
     });
   });
 });
