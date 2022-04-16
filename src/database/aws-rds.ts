17cb32607af42e3f04f4f77086465cff9b107cd5
Pablion <36828324+Pablion@users.noreply.github.com>
2022-04-16 07:39:56 +0200

    fix: aws-rds wrong function name

--- src/database/aws-rds.ts
+++ src/database/aws-rds.ts
@@ -9,30 +9,9 @@ export { pgAwsRdsConnectionTest };
 async function pgAwsRdsConnectionTest() {
   const client = new Client();
   await client.connect();
-
-  async function example_write() {
-    const res = await client.query('SELECT $1::text as message', [
-      'Hello world!',
-    ]);
-    console.log(res.rows[0].message); // Hello world!
-  }
-
-  async function example_read() {
-    var res;
-    try {
-      res = await client.query('SELECT $1::text as message', ['Hello world!']);
-      console.log('res.rows[0].message : ', res.rows[0].message); // DEV_LOG_TO_REMOVE
-      return res.rows[0].message;
-    } catch (err: any) {
-      console.log(err.stack); // Hello World!
-      throw new Error(err.stack);
-    }
-  }
-
-  await example_write();
-  const result = await example_read();
-
+  const res = await client.query('SELECT $1::text as message', [
+    'Hello world!',
+  ]);
   await client.end();
-
-  return result;
+  return res.rows[0].message;
 }
