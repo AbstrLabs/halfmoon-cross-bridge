76ff032355dcf18af36c4ff98e93436cc51479c6
Pablion <36828324+Pablion@users.noreply.github.com>
2022-05-01 05:26:45 +0200

    feat: yoctoNearToAtom

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -3,6 +3,7 @@ export {
   goNearToAtom,
   parseBurnApiParam,
   parseMintApiParam,
+  yoctoNearToAtom,
   type AlgoTxParam,
   type ApiCallParam,
   type NearTxParam,
@@ -13,6 +14,8 @@ import { BlockchainName, BridgeTxStatus, type BridgeTxInfo } from '..';
 import { ENV } from './dotenv';
 import { BridgeError, ERRORS } from './errors';
 import { z } from 'zod';
+import { logger } from './logger';
+import { utils } from 'near-api-js';
 
 type MintApiParam = z.infer<typeof mintApiParamParser>;
 type BurnApiParam = z.infer<typeof burnApiParamParser>;
@@ -114,8 +117,11 @@ const dbItemToBridgeTxInfo = (
 
 // goNear related
 function goNearToAtom(goNearPlain: string): string {
+  // TODO: typing: return value should be a BigInt
+
   // TODO: l10n: this only converts 1,234,567.0123456789 to 12345670123456789
   // TODO: l10n: and won't work for separators like 123_4567.0123456789 nor 1.234.567,0123456789
+  // TODO: l10n: temp-fix: added an regex to make sure that the input is in correct format
   // from https://github.com/near/near-api-js/blob/6f83d39f47624b4223746c0d27d10f78471575f7/src/utils/format.ts#L46-L53
 
   goNearPlain.replace(/,/g, '').trim(); // remove comma
@@ -127,9 +133,16 @@ function goNearToAtom(goNearPlain: string): string {
       goNearPlain,
     });
   }
-  return trimLeadingZeroes(
+  const atomAmount = trimLeadingZeroes(
     wholePart + fracPart.padEnd(ENV.GO_NEAR_DECIMALS, '0')
   );
+  logger.debug('goNearToAtom', {
+    goNearPlain,
+    wholePart,
+    fracPart,
+    atomAmount,
+  });
+  return atomAmount;
 }
 
 /**
@@ -144,4 +157,14 @@ function trimLeadingZeroes(value: string): string {
     return '0';
   }
   return value;
+}
+
+function yoctoNearToAtom(yoctoNear: string): bigint {
+  const nearPlain = utils.format.formatNearAmount(yoctoNear);
+  if (nearPlain === null) {
+    throw new BridgeError(ERRORS.INTERNAL.INVALID_YOCTO_NEAR_AMOUNT, {
+      yoctoNear,
+    });
+  }
+  return BigInt(goNearToAtom(nearPlain));
 }
