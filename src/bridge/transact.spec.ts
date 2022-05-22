d37adeb6c65b5a96e8a7674ee744959f658cd14f
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-05-22 06:18:45 +0200

    ref: merge test of mint and burn

--- src/bridge/transact.spec.ts
+++ src/bridge/transact.spec.ts
@@ -4,6 +4,8 @@ import { ApiCallParam } from '../utils/type';
 import { ENV } from '../utils/dotenv';
 import { FinalExecutionOutcome } from 'near-api-js/lib/providers';
 import { TxnType } from '../blockchain';
+import { testAlgo } from '../blockchain/algorand';
+import { toGoNearAtom } from '../utils/formatter';
 import { transact } from './transact';
 import { transferOnNearTestnetFromExampleToMaster } from './test-helper';
 
@@ -36,6 +38,48 @@ describe('mint test', () => {
       // should return AlgoTxnId,etc.
 
       // console.log('bridgeTxn : ', bridgeTxn);
+      // verification
+      expect(bridgeTxn.toTxnId).toBeDefined();
+    },
+    TIMEOUT_30S * 3
+  );
+  /* TODO: More tests:
+   * - wrong amount,
+   * - wrong txnId
+   * - malformed address
+   * - timeout (?override with jest?)
+   */
+});
+
+// TODO: mint-burn-test: move more same functions to test helper.
+
+describe('burn test', () => {
+  it(
+    'burn 1.2345678901 goNEAR from ALGO to NEAR',
+    async () => {
+      // config
+      const amount = '1.2345678901';
+
+      // simulate frontend: make NEAR txn
+      const burnResponse = await testAlgo.sendFromExampleToMaster(
+        toGoNearAtom(amount)
+      );
+      // manually checked the amount is correct.
+      const algoTxnId = burnResponse;
+
+      const apiCallParam: ApiCallParam = {
+        txnType: TxnType.BURN,
+        from: ENV.ALGO_EXAMPL_ADDR,
+        to: ENV.NEAR_EXAMPL_ADDR,
+        amount,
+        txnId: algoTxnId,
+      };
+
+      // call API
+      const bridgeTxn = await transact(apiCallParam);
+
+      // should return AlgoTxnId,etc.
+
       // verification
       expect(bridgeTxn.toTxnId).toBeDefined();
     },
