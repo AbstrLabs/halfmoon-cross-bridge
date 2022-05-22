ad023e90613d77303ae0cc98f493e959abddb265
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-05-22 06:14:35 +0200

    ref: unify `burn` and `transact` to rm later

--- src/bridge/burn.ts
+++ src/bridge/burn.ts
@@ -1,37 +1,53 @@
 /**
- * @exports burn - Create a {@link BridgeTxn} instance from {@link BurnApiParam} for burning and execute the transaction.
+ * @exports burn - Create a {@link BridgeTxn} instance from {@link ApiCallParam} for burning and execute the transaction.
  */
 export { burn };
 
+import { ApiCallParam, Stringer } from '../utils/type';
+import { BridgeError, ERRORS } from '../utils/errors';
 import { BridgeTxn, BridgeTxnObject } from '.';
 
-import { BurnApiParam } from '../utils/type';
 import { TxnType } from '../blockchain';
 import { literals } from '../utils/literals';
 import { logger } from '../utils/logger';
 
 /**
- * Create a {@link BridgeTxn} instance from {@link BurnApiParam} for burning and execute the transaction.
+ * Create a {@link BridgeTxn} instance from {@link ApiCallParam} for burning and execute the transaction.
  *
  * @async
- * @param  {BurnApiParam} burnApiParam
+ * @param  {ApiCallParam} apiCallParam
  * @returns {Promise<BridgeTxnObject>} A BridgeTxnObject representing the burn bridge transaction.
  */
-async function burn(burnApiParam: BurnApiParam): Promise<BridgeTxnObject> {
+async function burn(apiCallParam: ApiCallParam): Promise<BridgeTxnObject> {
+  let _literals: {
+    START: (amount: Stringer, from: Stringer, to: Stringer) => string;
+    DONE: string;
+  };
+  if (apiCallParam.txnType === TxnType.MINT) {
+    _literals = {
+      START: literals.START_MINTING,
+      DONE: literals.DONE_MINT,
+    };
+  } else if (apiCallParam.txnType === TxnType.BURN) {
+    _literals = {
+      START: literals.START_BURNING,
+      DONE: literals.DONE_BURN,
+    };
+  } else {
+    throw new BridgeError(ERRORS.INTERNAL.UNKNOWN_TXN_TYPE, {
+      at: 'transact',
+      apiCallParam,
+    });
+  }
   logger.info(
-    literals.START_BURNING(
-      burnApiParam.amount,
-      burnApiParam.from,
-      burnApiParam.to
-    )
+    _literals.START(apiCallParam.amount, apiCallParam.from, apiCallParam.to)
   );
   const bridgeTxn = BridgeTxn.fromApiCallParam(
-    burnApiParam,
-    TxnType.BURN,
+    apiCallParam,
     BigInt(Date.now())
   );
   const bridgeTxnObject = await bridgeTxn.runWholeBridgeTxn();
   // ERR handler .burn success
-  logger.info(literals.DONE_BURN);
+  logger.info(_literals.DONE);
   return bridgeTxnObject;
 }
--- src/bridge/transact.ts
+++ src/bridge/transact.ts
@@ -3,32 +3,51 @@
  */
 export { transact };
 
+import { ApiCallParam, Stringer } from '../utils/type';
+import { BridgeError, ERRORS } from '../utils/errors';
 import { BridgeTxn, BridgeTxnObject } from '.';
 
-import { MintApiParam } from '../utils/type';
+import { TxnType } from '../blockchain';
 import { literals } from '../utils/literals';
 import { logger } from '../utils/logger';
 
 /**
- * Create a {@link BridgeTxn} instance from {@link MintApiParam} for minting, and execute the transaction.
+ * Create a {@link BridgeTxn} instance from {@link ApiCallParam} for minting, and execute the transaction.
  *
  * @async
- * @param  {MintApiParam} mintApiParam
+ * @param  {ApiCallParam} apiCallParam
  * @returns {Promise<BridgeTxnObject>} A BridgeTxnObject representing the burn bridge transaction.
  */
-async function transact(mintApiParam: MintApiParam): Promise<BridgeTxnObject> {
+async function transact(apiCallParam: ApiCallParam): Promise<BridgeTxnObject> {
+  let _literals: {
+    START: (amount: Stringer, from: Stringer, to: Stringer) => string;
+    DONE: string;
+  };
+  if (apiCallParam.txnType === TxnType.MINT) {
+    _literals = {
+      START: literals.START_MINTING,
+      DONE: literals.DONE_MINT,
+    };
+  } else if (apiCallParam.txnType === TxnType.BURN) {
+    _literals = {
+      START: literals.START_BURNING,
+      DONE: literals.DONE_BURN,
+    };
+  } else {
+    throw new BridgeError(ERRORS.INTERNAL.UNKNOWN_TXN_TYPE, {
+      at: 'transact',
+      apiCallParam,
+    });
+  }
   logger.info(
-    literals.START_MINTING(
-      mintApiParam.amount,
-      mintApiParam.from,
-      mintApiParam.to
-    )
+    _literals.START(apiCallParam.amount, apiCallParam.from, apiCallParam.to)
   );
   const bridgeTxn = BridgeTxn.fromApiCallParam(
-    mintApiParam,
+    apiCallParam,
     BigInt(Date.now())
   );
   const bridgeTxnObject = await bridgeTxn.runWholeBridgeTxn();
-  logger.info(literals.DONE_MINT);
+  // ERR handler .burn success
+  logger.info(_literals.DONE);
   return bridgeTxnObject;
 }
