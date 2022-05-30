ecc5af65eca62de1eaf93715103ee969678abdbe
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-05-30 10:20:54 +0200

    feat: update `WELCOME_JSON` with version and HTTPS

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,15 +1,16 @@
 export { WELCOME_JSON };
 
-// import PackageJson from "../../package.json";
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
+const version = require('../../package.json').version as string;
+
 const WELCOME_JSON = {
   MESSAGE: 'Welcome to the Algorand-NEAR bridge API',
-  FRONTEND: 'http://halfmooncross.com/',
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-var-requires
-  API_VERSION: require('../../package.json').version,
-  API_SERVER: 'http://api.halfmooncross.com/',
+  FRONTEND: 'https://halfmooncross.com/',
+  API_VERSION: version,
+  API_SERVER: 'https://api.halfmooncross.com/',
   API_ENDPOINT: [
     {
-      URL: 'http://api.halfmooncross.com/algorand-near',
+      URL: 'https://api.halfmooncross.com/algorand-near',
       METHOD: 'POST',
       PARAMS: {
         type: 'literal("MINT","BURN")',
