e930619a6fabce89796cb4e0a684c07a3616a7da
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-03 12:30:25 +0200

    test: add a DB connection err handler

--- src/utils/test/global-beforeAll.ts
+++ src/utils/test/global-beforeAll.ts
@@ -1,12 +1,18 @@
 import { db } from '../../database/db';
 import { loadDotEnv } from '../dotenv';
+import { logger } from '../logger';
 
 loadDotEnv(); // better than calling `ENV`.
 const SECOND = 1000;
 jest.setTimeout(10 * SECOND); // in milliseconds
 
 beforeAll(async () => {
-  await db.connect();
+  try {
+    await db.connect();
+  } catch {
+    logger.error('Error connecting to database');
+    return;
+  }
 });
 afterAll(async () => {
   await db.end();
