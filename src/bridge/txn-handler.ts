3e3e208eb33e07ab0653457532038f10a3b4ebde
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-06 05:34:48 +0200

    feat: add a in-ram queue for `TxnHanlder`

--- src/bridge/txn-handler.ts
+++ src/bridge/txn-handler.ts
@@ -13,19 +13,28 @@ class TxnHandler {
   }
   /* private */ async _execute(bridgeTxn: BridgeTxn): Promise<BridgeTxnObj> {
     logger.warn('calling a methods that should be private');
+    // only handles fresh-new task
     return await bridgeTxn.runWholeBridgeTxn();
+    // TODO: support BridgeTxn with more txnStatus
   }
   /* async  */ loadUnfinishedTasksFromDb() {
     throw new Error('Function not implemented.');
   }
-  /* async  */ addTask() {
+  /* async  */ addTask(bridgeTxn: BridgeTxn) {
+    if (this._hasTask(bridgeTxn)) {
+      throw new Error('task already exists in TxnHandler queue');
+    }
+    this.queue.push(bridgeTxn);
+  }
+  /* private async */ handleTask() {
     throw new Error('Function not implemented.');
   }
-  /* async  */ handleTask() {
+  /* private async */ removeTask() {
     throw new Error('Function not implemented.');
   }
-  /* async  */ removeTask() {
-    throw new Error('Function not implemented.');
+
+  private _hasTask(bridgeTxn: BridgeTxn): boolean {
+    return this.queue.includes(bridgeTxn); // TODO: should compare UID here.
   }
 
   get length(): number {
