493aefdd83436e71ada0631ad46482262632f980
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-19 01:24:32 +0200

    test: add an example test with ESM to test PW

--- src/integration-test/example.spec.ts
+++ src/integration-test/example.spec.ts
@@ -1,7 +1,15 @@
 import { test, expect } from '@playwright/test';
+import { logger } from '../utils/logger.ts';
 
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
+});
+
+test('log "something"', () => {
+  // eslint-disable-next-line
+  logger.info('something');
+  console.log('something'); // this is better to show call stack
+  // TODO: override logger.info with console.log
 });
