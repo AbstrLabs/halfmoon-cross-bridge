59808198102b8849356649f357d002e20a155996
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-27 03:14:27 +0200

    ref: refactor "algorand-near.ts"

--- src/server/algorand-near.ts
+++ src/server/algorand-near.ts
@@ -24,6 +24,89 @@ algorandNear
     await handleAlgorandNearApiCall(req, res);
   });
 
+async function handleAlgorandNearApiCall(req: Request, res: Response) {
+  const apiCallParam = verifyApiCallParamWithResp(req, res);
+  if (apiCallParam === null) return;
+
+  const verifyResult = await verifyBlockchainTxnWithResp(apiCallParam, res);
+  if (verifyResult === null) return;
+
+  // TRANSACT
+  try {
+    await transactWithResp(apiCallParam, res);
+  } catch (err) {
+    logger.error(err);
+    res.status(500).send('Internal server error.');
+    res.end();
+    logger.info('API server still running...');
+    return;
+  }
+}
+
+function verifyApiCallParamWithResp(
+  req: Request,
+  res: Response
+): ApiCallParam | null {
+  try {
+    const body = req.body as {
+      type: TxnType;
+      from: string;
+      to: string;
+      amount: string;
+      txnId: string;
+    };
+    const apiCallParam = parseApiCallParam(body);
+    return apiCallParam;
+  } catch (err) {
+    res.status(406).send('Wrong POST body');
+    return null;
+  }
+}
+
+/**
+ *
+ * @todo - verify within both ram and db.
+ *
+ * @param apiCallParam
+ * @param res
+ * @returns
+ */
+async function verifyBlockchainTxnWithResp(
+  apiCallParam: ApiCallParam,
+  res: Response
+): Promise<ConfirmOutcome.SUCCESS | null> {
+  const verifyBlockchainMap = {
+    [TxnType.MINT]: BlockchainName.NEAR,
+    [TxnType.BURN]: BlockchainName.ALGO,
+  };
+
+  let verifyResult: ConfirmOutcome;
+  try {
+    verifyResult = await verifyBlockchainTxn(
+      apiCallParam,
+      verifyBlockchainMap[apiCallParam.type]
+    );
+  } catch (err) {
+    res.status(400).send('Invalid transaction');
+    return null;
+  }
+
+  switch (verifyResult) {
+    case ConfirmOutcome.SUCCESS:
+      logger.verbose('verifyBlockchainTxnWithResp: passed');
+      return ConfirmOutcome.SUCCESS;
+    case ConfirmOutcome.WRONG_INFO:
+      res.status(406).send('Invalid transaction');
+      return null;
+    case ConfirmOutcome.TIMEOUT:
+      res.status(500).send('Verify transaction time out');
+      return null;
+    default:
+      res.status(500).send('Server error code ANB-001');
+      return null;
+  }
+}
+
 async function transactWithResp(apiCallParam: ApiCallParam, res: Response) {
   /* CONFIG */
   let bridgeTxnObject: BridgeTxnObj;
@@ -52,81 +135,4 @@ async function transactWithResp(apiCallParam: ApiCallParam, res: Response) {
     'API call ended, returned:\n' + JSON.stringify(stringifiedBridgeTxnObject)
   );
   return res.json(stringifiedBridgeTxnObject);
-}
-
-async function handleAlgorandNearApiCall(req: Request, res: Response) {
-  let apiCallParam: ApiCallParam | undefined = undefined;
-
-  // VERIFY API CALL PARAM
-  try {
-    const body = req.body as {
-      type: TxnType;
-      from: string;
-      to: string;
-      amount: string;
-      txnId: string;
-    };
-    apiCallParam = parseApiCallParam(body);
-  } catch (err) {
-    res.status(406).send('Wrong POST body');
-    res.end();
-    logger.info('API server still running...');
-    return;
-  }
-
-  // VERIFY INCOMING TXN
-  await verifyBlockchainTxnWithResp(apiCallParam, res);
-  // TODO: verify within both ram and db.
-
-  // TRANSACT
-  try {
-    await transactWithResp(apiCallParam, res);
-  } catch (err) {
-    logger.error(err);
-    res.status(500).send('Internal server error.');
-    res.end();
-    logger.info('API server still running...');
-    return;
-  }
-}
-
-async function verifyBlockchainTxnWithResp(
-  apiCallParam: ApiCallParam,
-  res: Response
-) {
-  const verifyBlockchainMap = {
-    [TxnType.MINT]: BlockchainName.NEAR,
-    [TxnType.BURN]: BlockchainName.ALGO,
-  };
-
-  let verifyResult: ConfirmOutcome;
-  try {
-    verifyResult = await verifyBlockchainTxn(
-      apiCallParam,
-      verifyBlockchainMap[apiCallParam.type]
-    );
-  } catch (err) {
-    res.status(400).send('Invalid transaction');
-    res.end();
-    logger.info('API server still running...');
-    return;
-  }
-  switch (verifyResult) {
-    case ConfirmOutcome.SUCCESS:
-      logger.info('Verify success');
-      break;
-    case ConfirmOutcome.WRONG_INFO:
-      res.status(406).send('Invalid transaction');
-      res.end();
-      logger.info('API server still running...');
-      return;
-    case ConfirmOutcome.TIMEOUT:
-      res.status(500).send('Verify transaction time out');
-      res.end();
-      logger.info('API server still running...');
-      return;
-    default:
-      res.status(500).send('Server error code ANB-001');
-      return;
-  }
 }
