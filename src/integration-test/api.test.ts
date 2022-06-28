8cbc6d33594e4cd94f9c280be531137f001d4a44
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-29 01:22:01 +0200

    chore: use jest for integration test

--- src/integration-test/api.test.ts
+++ src/integration-test/api.test.ts
@@ -1,8 +1,42 @@
-import { test, expect } from '@playwright/test';
+import { IncomingMessage, request, RequestOptions } from 'node:http';
 
-test('host API server', async ({ request }) => {
-  const res = await request.get('./');
-  expect(res.ok()).toBeTruthy();
+it('host API server', async () => {
+  // expect(res.statusMessage).toBeTruthy();
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  expect((await res.json()).API_VERSION == '0.1.1').toBe(true);
+  // expect((await res.json()).API_VERSION == '0.1.1').toBe(true);
+  await new Promise((resolve) => {
+    const reqOpt: RequestOptions = {
+      // host: '',
+      method: 'GET',
+    };
+    const req = request(
+      'http://localhost:4190/',
+      reqOpt,
+      (res: IncomingMessage) => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        console.log(`STATUS: ${res.statusCode!.toString()}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk: string) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+          console.log('No more data in response.');
+          resolve(null);
+        });
+      }
+    );
+
+    req.end();
+  });
 });
+
+// const resp = await request("/", {
+//   method: 'POST',
+//   mode: 'cors',
+//   body: JSON.stringify(postParam),
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Content-Length': `${Buffer.byteLength(JSON.stringify(postParam))}`,
+//   },
+// });
