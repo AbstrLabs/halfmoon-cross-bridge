1bb252b13c21ca1f6b8a3ceeea204754e2f04d76
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-29 02:32:52 +0200

    test: finish one jest API basic test

--- src/integration-test/api.test.ts
+++ src/integration-test/api.test.ts
@@ -1,35 +1,45 @@
 import { IncomingMessage, request, RequestOptions } from 'node:http';
+import { WELCOME_JSON } from '../server';
 
-it('host API server', async () => {
-  // expect(res.statusMessage).toBeTruthy();
+it('hosted API server returns welcome JSON on GET', async () => {
+  //
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   // expect((await res.json()).API_VERSION == '0.1.1').toBe(true);
+  let message: string | undefined = undefined;
   await new Promise((resolve) => {
+    const reqCb = (res: IncomingMessage) => {
+      expect(res.statusCode).toBe(200);
+      expect(res.statusMessage).toBe('OK');
+      // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+      res.on('data', (chunk: Buffer) => {
+        if (message !== undefined) {
+          console.warn('received more than one chunk');
+        }
+        message = chunk.toString('utf8');
+      });
+      res.on('end', () => {
+        if (message === undefined) {
+          console.error('no message received');
+          throw Error('no message received');
+        }
+        expect(JSON.parse(message)).toStrictEqual(WELCOME_JSON);
+        resolve(null);
+      });
+    };
+
     const reqOpt: RequestOptions = {
-      // host: '',
+      host: 'localhost',
+      port: 4190,
       method: 'GET',
     };
-    const req = request(
-      'http://localhost:4190/',
-      reqOpt,
-      (res: IncomingMessage) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        console.log(`STATUS: ${res.statusCode!.toString()}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk: string) => {
-          console.log(`BODY: ${chunk}`);
-        });
-        res.on('end', () => {
-          console.log('No more data in response.');
-          resolve(null);
-        });
-      }
-    );
-
+    const req = request(reqOpt, reqCb);
     req.end();
   });
 });
+
+// it('/algorand-near creates transaction in database on POST', async () => {
+
+// });
 
 // const resp = await request("/", {
 //   method: 'POST',
