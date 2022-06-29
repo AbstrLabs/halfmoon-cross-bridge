dc64001b199a68f05ec8e054be774df2ab076531
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-29 05:34:26 +0200

    chore: use axios in integration-test

--- src/test/integration-test/api.test.ts
+++ src/test/integration-test/api.test.ts
@@ -1,40 +1,11 @@
-import { IncomingMessage, request, RequestOptions } from 'node:http';
+import axios from 'axios';
 import { WELCOME_JSON } from '../../server';
 
 it('hosted API server returns welcome JSON on GET', async () => {
-  //
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  // expect((await res.json()).API_VERSION == '0.1.1').toBe(true);
-  let message: string | undefined = undefined;
-  await new Promise((resolve) => {
-    const reqCb = (res: IncomingMessage) => {
-      expect(res.statusCode).toBe(200);
-      expect(res.statusMessage).toBe('OK');
-      // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-      res.on('data', (chunk: Buffer) => {
-        if (message !== undefined) {
-          console.warn('received more than one chunk');
-        }
-        message = chunk.toString('utf8');
-      });
-      res.on('end', () => {
-        if (message === undefined) {
-          console.error('no message received');
-          throw Error('no message received');
-        }
-        expect(JSON.parse(message)).toStrictEqual(WELCOME_JSON);
-        resolve(null);
-      });
-    };
-
-    const reqOpt: RequestOptions = {
-      host: 'localhost',
-      port: 4190,
-      method: 'GET',
-    };
-    const req = request(reqOpt, reqCb);
-    req.end();
-  });
+  const res = await axios.get('http://localhost:4190/');
+  expect(res.data).toStrictEqual(WELCOME_JSON);
+  expect(res.status).toBe(200);
+  expect(res.statusText).toBe('OK');
 });
 
 // it('/algorand-near creates transaction in database on POST', async () => {
