cecd4843a259896051ee041bb85ae229c84134a7
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-30 05:07:08 +0200

    test: add /algorand-near GET with (in/)valid UID

--- src/test/integration-test/api.test.ts
+++ src/test/integration-test/api.test.ts
@@ -32,34 +32,48 @@ it('/algorand-near returns welcome JSON on no-param GET', async () => {
   expect(res.statusText).toBe('OK');
 });
 
-it('/algorand-near returns welcome JSON on valid-param-ed GET', async () => {
-  // TODO: finish this test
-  const res = await axios.get('http://localhost:4190/algorand-near');
+it('/algorand-near returns welcome JSON on VALID-param-ed GET', async () => {
+  // TODO: [TEST-CONST]
+  const EXAMPLE_UID_FROM_DB =
+    '57.2HXYPGDY2EDVERXXQH6UKAT22EQGXWGWPWSJFY3G22AQLNZYTTDA';
+  // TODO: [TEST-CONST]
+  const EXAMPLE_RESULT_FROM_DB = {
+    createdTime: '1656171676417',
+    dbId: 57,
+    fixedFeeAtom: '10000000000',
+    fromAddr: 'ACCSSTKTJDSVP4JPTJWNCGWSDAPHR66ES2AZUAH7MUULEY43DHQSDNR7DA',
+    fromAmountAtom: '12345678901',
+    fromBlockchainName: 'ALGO',
+    fromTxnId: '2HXYPGDY2EDVERXXQH6UKAT22EQGXWGWPWSJFY3G22AQLNZYTTDA',
+    marginFeeAtom: '24691358',
+    toAddr: 'abstrlabs-test.testnet',
+    toAmountAtom: '2320987543',
+    toBlockchainName: 'NEAR',
+    toTxnId: '2VE7QxZZ92PKGkzJzbhf44MTeoxU4LBGXSgXVVAYHNee',
+    txnStatus: 'DONE_INITIALIZE',
+    txnType: 'BURN',
+  };
 
-  const IGNORED = 'IGNORED';
-  interface shaped {
-    ADDITIONAL_INFO: { SERVER_UP_TIMESTAMP: string };
-  }
-  (res.data as shaped).ADDITIONAL_INFO.SERVER_UP_TIMESTAMP = IGNORED;
-  (WELCOME_JSON as shaped).ADDITIONAL_INFO.SERVER_UP_TIMESTAMP = IGNORED;
-  expect(res.data).toStrictEqual(WELCOME_JSON);
+  const res = await axios.get(
+    'http://localhost:4190/algorand-near?uid=' + EXAMPLE_UID_FROM_DB
+  );
+
+  console.log(res.data);
+  expect(res.data).toStrictEqual(EXAMPLE_RESULT_FROM_DB);
   expect(res.status).toBe(200);
   expect(res.statusText).toBe('OK');
 });
 
 it('/algorand-near returns 406 invalid-param-ed GET', async () => {
-  // TODO: finish this test
-  const res = await axios.get('http://localhost:4190/algorand-near');
+  // TODO: [TEST-CONST]
+  const EXAMPLE_MALFORMED_UID_FROM_DB =
+    '58.2HXYPGDY2EDVERXXQH6UKAT22EQGXWGWPWSJFY3G22AQLNZYTTDA';
 
-  const IGNORED = 'IGNORED';
-  interface shaped {
-    ADDITIONAL_INFO: { SERVER_UP_TIMESTAMP: string };
-  }
-  (res.data as shaped).ADDITIONAL_INFO.SERVER_UP_TIMESTAMP = IGNORED;
-  (WELCOME_JSON as shaped).ADDITIONAL_INFO.SERVER_UP_TIMESTAMP = IGNORED;
-  expect(res.data).toStrictEqual(WELCOME_JSON);
-  expect(res.status).toBe(200);
-  expect(res.statusText).toBe('OK');
+  await expect(
+    axios.get(
+      'http://localhost:4190/algorand-near?uid=' + EXAMPLE_MALFORMED_UID_FROM_DB
+    )
+  ).rejects.toThrowError(new AxiosError('Request failed with status code 406'));
 });
 
 it('/algorand-near creates transaction in database on POST', async () => {
