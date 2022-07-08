fa20439c7f2ec1713ed39801708c5e9c2f1b7ae6
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-07-08 08:33:26 +0200

    test: add test for parseAlgoAddr

--- src/utils/types.spec.ts
+++ src/utils/types.spec.ts
@@ -1,20 +1,29 @@
-import { EXAMPLE_API_PARAM } from '../test/test-helper/test-examples';
-import { fullyParseApiParam } from './type';
+import {
+  EXAMPLE_ALGO_ADDR,
+  EXAMPLE_API_PARAM,
+} from '../test/test-helper/test-examples';
+import { fullyParseApiParam, parseAlgoAddr } from './type';
 
 describe('type.ts', () => {
-  describe('fullyParseApiParam', () => {
-    it('should parse correct API param', () => {
-      expect(() => {
-        fullyParseApiParam(EXAMPLE_API_PARAM);
-      }).not.toThrow();
+  describe('Zod Parsers', () => {
+    describe('parseAlgoAddr', () => {
+      it('should parse correct Algo Addr', () => {
+        const parsedAddr = parseAlgoAddr(EXAMPLE_ALGO_ADDR);
+        expect(parsedAddr).toEqual(EXAMPLE_ALGO_ADDR);
+      });
     });
-    it('should reject wrong API param', () => {
-      expect(() => {
-        const copy = { ...EXAMPLE_API_PARAM };
-        // copy.to_id = 3; TS handles this
-        copy.to_addr = 'wrong';
-        fullyParseApiParam(copy);
-      }).toThrow();
+    describe('fullyParseApiParam', () => {
+      it('should parse correct API param', () => {
+        expect(fullyParseApiParam(EXAMPLE_API_PARAM)).toBe(EXAMPLE_API_PARAM);
+      });
+      it('should reject wrong API param', () => {
+        expect(() => {
+          const copy = { ...EXAMPLE_API_PARAM };
+          // copy.to_id = 3; TS handles this
+          copy.to_addr = 'wrong';
+          fullyParseApiParam(copy);
+        }).toThrow();
+      });
     });
   });
 });
