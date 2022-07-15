62f5ebd367efdfdbeb3e504eafdb59a5a08ad0da
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-07-15 20:12:50 +0200

    chore: add script `yarn doc` to gen TSDoc

--- src/api/docs.ts
+++ src/api/docs.ts
@@ -4,8 +4,5 @@ import express from 'express';
 const docsRoute = express.Router();
 
 docsRoute.route('/docs');
-// .get((req: Request, res: Response) => {
-//   res.sendFile(`${__dirname}/../../docs/index.html`);
-// });
 
-docsRoute.use('/docs', express.static(`${__dirname}/../../docs`));
+docsRoute.use('/docs', express.static(`${__dirname}/../../TSDoc`));
