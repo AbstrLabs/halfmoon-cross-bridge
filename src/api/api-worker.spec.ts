012a33509e99e227936331c9d283a4d946ad267a
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-07-21 00:50:09 +0200

    ref: use `CriticalApiCallParam` in `ApiWorker`

---
+++ src/api/api-worker.spec.ts
@@ -0,0 +1,24 @@
+import { BridgeTxn } from '../bridge';
+import {
+  EXAMPLE_ALGO_ADDR,
+  EXAMPLE_NEAR_ADDR,
+} from '../test/test-helper/test-examples';
+import { TokenId } from '../utils/type/shared-types/token';
+import { ApiCallParam } from '../utils/type/type';
+import { apiWorker } from './api-worker';
+
+describe('API Worker should', () => {
+  /** skipped for txn_id is added and would cause problem */
+  it.skip('create a BridgeTxn from API call params', async () => {
+    const apiCallParam: ApiCallParam = {
+      from_token: TokenId.NEAR,
+      to_token: TokenId.goNEAR,
+      txn_id: 'txn_id',
+      amount: '1',
+      from_addr: EXAMPLE_NEAR_ADDR,
+      to_addr: EXAMPLE_ALGO_ADDR,
+    };
+    const bridgeTxn = await apiWorker.create(apiCallParam);
+    expect(bridgeTxn).toBeInstanceOf(BridgeTxn);
+  });
+});
