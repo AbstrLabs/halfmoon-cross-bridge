17c24753cf68246d974a22aaf3c0c4ca608b869c
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-07-21 06:04:45 +0200

    ref: add some util types

--- src/utils/type/type.ts
+++ src/utils/type/type.ts
@@ -48,6 +48,28 @@ interface Stringer {
   toString(): string;
 }
 
+/* TYPE OPERATORS */
+// TODO: ref: split zod type and type operators into separate files
+
+export type PartiallyRequired<T, Keys extends keyof T> = Required<
+  Pick<T, Keys>
+> &
+  T;
+
+export type Expand<T> = T extends (...args: infer A) => infer R
+  ? (...args: Expand<A>) => Expand<R>
+  : T extends infer O
+  ? { [K in keyof O]: O[K] }
+  : never;
+
+export type ExpandRecursively<T> = T extends (...args: infer A) => infer R
+  ? (...args: ExpandRecursively<A>) => ExpandRecursively<R>
+  : T extends object
+  ? T extends infer O
+    ? { [K in keyof O]: ExpandRecursively<O[K]> }
+    : never
+  : T;
+
 /* ZOD TYPES (WITH PARSER) */
 
 /**
