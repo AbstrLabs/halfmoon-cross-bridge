7775804b93526509c3879099ba8268a3e0c82ca0
PabloLION <36828324+PabloLION@users.noreply.github.com>
2022-07-24 18:35:13 +0200

    ref: ren EXAMPLE_BURN_TXN_FROM

--- src/bridge/bridge-txn.spec.ts
+++ src/bridge/bridge-txn.spec.ts
@@ -1,17 +1,17 @@
-import { EXAMPLE_TXN_FROM_DB } from '../test/test-helper/test-examples';
+import { EXAMPLE_BURN_TXN_FROM_DB } from '../test/test-helper/test-examples';
 import { BridgeTxn } from './bridge-txn';
 
 describe('Class BridgeTxn should', () => {
   it('equals between two instances with same attrs', () => {
-    const bridgeTxn1 = BridgeTxn.fromObject(EXAMPLE_TXN_FROM_DB);
-    const bridgeTxn2 = BridgeTxn.fromObject(EXAMPLE_TXN_FROM_DB);
+    const bridgeTxn1 = BridgeTxn.fromObject(EXAMPLE_BURN_TXN_FROM_DB);
+    const bridgeTxn2 = BridgeTxn.fromObject(EXAMPLE_BURN_TXN_FROM_DB);
     expect(bridgeTxn1.toString() === bridgeTxn2.toString()).toBe(true);
     expect(bridgeTxn1 === bridgeTxn2).toBe(false);
   });
 
   it('calculates margin fee correctly', () => {
     const txn = {
-      ...EXAMPLE_TXN_FROM_DB,
+      ...EXAMPLE_BURN_TXN_FROM_DB,
     };
     const bridgeTxn = new BridgeTxn({
       fromAddr: txn.fromAddr,
