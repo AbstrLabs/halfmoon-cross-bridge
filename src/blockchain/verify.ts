adcaf860db20db7389c7cc5953ee7d40e18fb2ea
PabloLION <36828324+PabloLION@users.noreply.github.com>
2022-07-24 22:59:03 +0200

    ref: clean 'verify'

--- src/blockchain/verify.ts
+++ src/blockchain/verify.ts
@@ -6,30 +6,15 @@ export { verifyBlockchainTxn };
 
 import { ApiCallParam, TxnParam } from '../utils/type/type';
 
-import { BlockchainName } from '..';
-import { ConfirmOutcome, type Blockchain } from './abstract-base';
-import { nearBlockchain } from './near';
-import { algoBlockchain } from './algorand';
-import { TOKEN_TABLE } from '../bridge/token-table';
+import { ConfirmOutcome } from './abstract-base';
+import { getTokenImplBlockchain } from '../bridge/token-table';
 import { getBridgeInfo } from '../bridge/bridge-info';
 
 async function verifyBlockchainTxn(
   apiCallParam: ApiCallParam
 ): Promise<ConfirmOutcome> {
-  let blockchain: Blockchain;
-  const blockchainName = TOKEN_TABLE[apiCallParam.from_token].implBlockchain;
-  // TODO: use match.
-  if (blockchainName === BlockchainName.ALGO) {
-    blockchain = algoBlockchain;
-    // for extendability, we can add more blockchain names here.
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  } else if (blockchainName === BlockchainName.NEAR) {
-    blockchain = nearBlockchain;
-  } else {
-    // for extendability, we can add more blockchain names here.
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    throw new Error(`Blockchain ${blockchainName} is not supported.`);
-  }
+  const blockchain = getTokenImplBlockchain(apiCallParam.from_token);
+
   const txnParam: TxnParam = {
     fromAddr: apiCallParam.from_addr,
     toAddr: blockchain.centralizedAddr,
@@ -41,6 +26,4 @@ async function verifyBlockchainTxn(
   };
 
   return await blockchain.confirmTxn(txnParam);
-  // const expectedTxnOutcome = await blockchain.getTxnStatus(txnParam);
-  // blockchain.verifyCorrectness(expectedTxnOutcome, txnParam);
 }
