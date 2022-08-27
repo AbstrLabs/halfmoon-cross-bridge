b1f16533e2ad213605cac638bd0c4c5d50f335e9
Bo Yao <bo@abstrlabs.com>
2022-08-27 23:15:21 +0800

    algo createTransactionObject

--- worker/src/blockchain/algorand.ts
+++ worker/src/blockchain/algorand.ts
@@ -1,18 +1,28 @@
 import { Blockchain, FromToken, FromTxn, Transaction, TransactionParams, TransactionStatus, VerifyResult } from "./base";
-import { Algodv2 as AlgodClient, Indexer, decodeAddress } from 'algosdk';
+import { Algodv2 as AlgodClient, Indexer } from 'algosdk';
+import * as algosdk from 'algosdk';
+
 import { env } from "../utils";
 import base58, { decode } from "bs58";
 
 class AlgoTransaction extends Transaction {
-
+  constructor(public txn_hash: string, public txn_bytes: Uint8Array) {
+    super();
+  }
 }
 
 class AlgoBlockchain extends Blockchain{
     txnGoThroughTime = 5;
+    client = new AlgodClient({
+      'X-API-KEY': env('PURE_STAKE_API_KEY')
+    }, env('PURE_STAKE_ALGOD_URL'), '');
     indexer = new Indexer({
         'X-API-KEY': env('PURE_STAKE_API_KEY')
-    }, env('PURE_STAKE_INDEXER_URL'));
+    }, env('PURE_STAKE_INDEXER_URL'), '');
     centralizedAddr = env('ALGO_MASTER_ADDR');
+    centralizedSk = algosdk.mnemonicToSecretKey(
+      env('ALGO_MASTER_PASS')
+    ).sk;
 
     async verifyIncomingTransaction(fromTxn: FromTxn, fromToken: FromToken): Promise<VerifyResult> {
         let outcome;
@@ -31,7 +41,7 @@ class AlgoBlockchain extends Blockchain{
         const sender = txn.sender;
         const receiver = txn['asset-transfer-transaction'].receiver;
         const assetId = txn['asset-transfer-transaction']['asset-id'];
-        const pk = decodeAddress(sender).publicKey
+        const pk = algosdk.decodeAddress(sender).publicKey
     
         // verify confirmed
         if (!(currentRound >= confirmedRound)) {
@@ -59,19 +69,44 @@ class AlgoBlockchain extends Blockchain{
 
     async addressIsValid(addr: string): Promise<boolean> {
         try {
-          decodeAddress(addr)
+          algosdk.decodeAddress(addr)
         } catch (e) {
           return false
         }
         return true
     }
-    
-    async createTransactionObject(params: TransactionParams): Promise<AlgoTransaction> {
-      return new AlgoTransaction();
+
+    async createTransactionObject(algoTxnParam: TransactionParams): Promise<AlgoTransaction> {
+      const params = await this.client.getTransactionParams().do();
+      // comment out the next two lines to use suggested fee
+      // params.fee = algosdk.ALGORAND_MIN_TXN_FEE;
+      // params.flatFee = true;
+      // const enc = new TextEncoder();
+      // const note = enc.encode('Hello World');
+      const txnConfig = {
+        to: algoTxnParam.to_addr,
+        from: this.centralizedAddr,
+        amount: BigInt(algoTxnParam.to_amount_atom),
+        note: undefined, // maybe write the incoming txnId here
+        suggestedParams: params,
+        assetIndex: Number(algoTxnParam.to_token_addr),
+        revocationTarget: undefined,
+        closeRemainderTo: undefined,
+      };
+  
+      const txn =
+        algosdk.makeAssetTransferTxnWithSuggestedParamsFromObject(txnConfig);
+  
+      // Sign the transaction
+      const txnId = txn.txID().toString();
+      const rawSignedTxn = txn.signTxn(this.centralizedSk);   
+      return new AlgoTransaction(txnId, rawSignedTxn) 
     }
+    
     async sendTransaction(t: Uint8Array): Promise<void> {
         
     }
+    
     async checkTransactionStatus(txn_hash: string): Promise<TransactionStatus> {
         return TransactionStatus.NotExist;
     }
