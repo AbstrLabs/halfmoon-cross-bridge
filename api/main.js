0f143a77adbc7f716b19e3f196fa004a7af891f5
Bo Yao <bo@abstrlabs.com>
2022-10-02 15:26:21 +0800

    add dotenv for api

--- api/main.js
+++ api/main.js
@@ -1,8 +1,9 @@
-const app = require('./app')
-const log = require('halfmoon-cross-bridge-common/logger')
+require("dotenv").config();
+const app = require("./app");
+const log = require("halfmoon-cross-bridge-common/logger");
 
 const port = process.env.API_PORT ? Number(process.env.API_PORT) : 4190;
 
 app.listen(port, () => {
-    log.info(`Bridge API started on http://localhost:${port}/`)
+    log.info(`Bridge API started on http://localhost:${port}/`);
 });
